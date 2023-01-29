b885d535377c914099246d4ba247026cc034f3e6
«Natalia <kolodina_melbyk@mail.ru>
2023-01-29 13:03:34 +0400

    add example003 find positiont

---
+++ Example003FindPosition/Program.cs
@@ -0,0 +1,19 @@
+﻿// Поиск позиции (индекса) нужного нам элемента массива
+
+int[] array = { 12, 62, 13, 994, 55, 712, 6345, 712, 8 };
+
+int n = array.Length;
+int find = 712;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
